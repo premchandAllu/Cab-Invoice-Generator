202d0b5195158d29741264e6982452a999e97d9b
premchandAllu <premchandallu1999@gmail.com>
2020-10-21 21:36:57 +0530

    Added: Welcome to Cab Invoice Program

---
+++ main/java/com/blz/cabInvoiceGenerator/InvoiceGenerator.java
@@ -0,0 +1,9 @@
+package com.blz.cabInvoiceGenerator;
+
+public class InvoiceGenerator {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Cab invoice service");
+	}
+		
+}
