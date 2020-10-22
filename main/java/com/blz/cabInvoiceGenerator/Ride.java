13d5f8688c05dbb0b4bed96c5c45423896f7afa3
premchandAllu <premchandallu1999@gmail.com>
2020-10-22 11:26:23 +0530

    Added: Method for premium ride type

--- main/java/com/blz/cabInvoiceGenerator/Ride.java
+++ main/java/com/blz/cabInvoiceGenerator/Ride.java
@@ -1,12 +1,13 @@
 package com.blz.cabInvoiceGenerator;
 
 public class Ride {
-
 	public double distance;
 	public int time;
+	public String rideType;
 
-	public Ride(double distance, int time) {
+	public Ride(double distance, int time, String rideType) {
 		this.distance = distance;
 		this.time = time;
+		this.rideType = rideType;
 	}
 }
--- test/java/com/blz/cabInvoiceGenerator/InvoiceServiceTest.java
+++ test/java/com/blz/cabInvoiceGenerator/InvoiceServiceTest.java
@@ -1,5 +1,6 @@
 package com.blz.cabInvoiceGenerator;
 
+
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
@@ -17,7 +18,8 @@ public class InvoiceServiceTest {
 	public void givenDistanceAndTimeShouldReturnTotalFare() {
 		double distance = 2.5;
 		int time = 10;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		String rideType = "normal";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
 		Assert.assertEquals(35, fare, 0.0);
 	}
 
@@ -25,17 +27,26 @@ public class InvoiceServiceTest {
 	public void givenLessDistanceAndTimeShouldReturnMinimumFare() {
 		double distance = 0.1;
 		int time = 1;
-		double fare = invoiceGenerator.calculateFare(distance, time);
+		String rideType = "normal";
+		double fare = invoiceGenerator.calculateFare(distance, time, rideType);
 		Assert.assertEquals(5, fare, 0.0);
 	}
 
 	@Test
-	public void givenMultipleRidesShouldReturnInvoiceSummary() {
+	public void givenUserIdShouldReturnInvoiceSummary() {
 		int userId = 123;
-		Ride[] rides = { new Ride(2.0, 5), new Ride(0.1, 1) };
+		Ride[] rides = { new Ride(2.0, 5, "normal"), new Ride(0.1, 1, "premium") };
 		InvoiceGenerator cabInvoiceGenerator = new InvoiceGenerator(userId, rides);
 		InvoiceSummary actualInvoiceSummary = cabInvoiceGenerator.calculateFare(userId);
-		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 30.0);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 45.0);
 		Assert.assertEquals(expectedInvoiceSummary, actualInvoiceSummary);
+	}
+
+	@Test
+	public void givenMultipleRidesShouldReturnInvoiceSummary() {
+		Ride[] rides = { new Ride(2.0, 5, "normal"), new Ride(0.1, 1, "premium") };
+		InvoiceSummary invoiceSummary = invoiceGenerator.calculateFare(rides);
+		InvoiceSummary expectedInvoiceSummary = new InvoiceSummary(2, 45.0);
+		Assert.assertEquals(expectedInvoiceSummary, invoiceSummary);
 	}
 }
