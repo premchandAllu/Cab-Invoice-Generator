80709ba8a844ff149dd0e6c846937b683764ea37
premchandAllu <premchandallu1999@gmail.com>
2020-10-22 11:02:13 +0530

    Added: Method to give list of rides for given user

--- main/java/com/blz/cabInvoiceGenerator/InvoiceGenerator.java
+++ main/java/com/blz/cabInvoiceGenerator/InvoiceGenerator.java
@@ -6,12 +6,23 @@ public class InvoiceGenerator {
 	private static final int COST_PER_TIME = 1;
 	private static final double MINIMUM_FARE = 5.0;
 
+	private final RideRepository USER_RIDES;
+
+	public InvoiceGenerator() {
+		this.USER_RIDES = null;
+	}
+
+	public InvoiceGenerator(int userId, Ride[] rides) {
+		USER_RIDES = new RideRepository(userId, rides);
+	}
+
 	public double calculateFare(double distance, int time) {
 		double totalFare = distance * MINIMUM_COST_PER_KM + time * COST_PER_TIME;
 		return Math.max(totalFare, MINIMUM_FARE);
 	}
 
-	public InvoiceSummary calculateFare(Ride[] rides) {
+	public InvoiceSummary calculateFare(int userId) {
+		Ride[] rides = USER_RIDES.getRides(userId);
 		double totalFare = 0.0;
 		for (Ride ride : rides) {
 			totalFare += this.calculateFare(ride.distance, ride.time);
